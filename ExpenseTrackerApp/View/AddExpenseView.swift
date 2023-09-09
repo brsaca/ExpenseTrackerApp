8db3d2bbc01e4343473107975f088f9a7c0e6d3d
brsaca <brsaca@gmail.com>
2023-09-09 14:34:22 -0600

    feat: create AddExpenseView

---
+++ ExpenseTrackerApp/View/AddExpenseView.swift
@@ -0,0 +1,18 @@
+//
+//  AddExpenseView.swift
+//  ExpenseTrackerApp
+//
+//  Created by Brenda Saavedra Cantu on 09/09/23.
+//
+
+import SwiftUI
+
+struct AddExpenseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddExpenseView()
+}
