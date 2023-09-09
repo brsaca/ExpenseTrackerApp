ce1fb7f7e1dae6e9e2ba2fb487d63e31321ef85f
brsaca <brsaca@gmail.com>
2023-09-09 13:43:12 -0600

    Initial Commit

---
+++ ExpenseTrackerApp/ExpenseTrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerAppApp.swift
+//  ExpenseTrackerApp
+//
+//  Created by Brenda Saavedra Cantu on 09/09/23.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
