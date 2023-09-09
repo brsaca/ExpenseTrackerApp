447267ab91ab29c725b846d8725763be1c51faf6
brsaca <brsaca@gmail.com>
2023-09-09 14:12:57 -0600

    feat: add tabview and expenses and categories initial views

---
+++ ExpenseTrackerApp/View/CategoriesView.swift
@@ -0,0 +1,18 @@
+//
+//  CategoriesView.swift
+//  ExpenseTrackerApp
+//
+//  Created by Brenda Saavedra Cantu on 09/09/23.
+//
+
+import SwiftUI
+
+struct CategoriesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoriesView()
+}
---
+++ ExpenseTrackerApp/View/ExpensesView.swift
@@ -0,0 +1,18 @@
+//
+//  ExpensesView.swift
+//  ExpenseTrackerApp
+//
+//  Created by Brenda Saavedra Cantu on 09/09/23.
+//
+
+import SwiftUI
+
+struct ExpensesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExpensesView()
+}
